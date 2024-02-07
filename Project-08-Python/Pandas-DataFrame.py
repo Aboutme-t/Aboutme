9bb7deb1d0d644a3f7ebec797bce5f29f97017fe
Kaitkong  Kongpien <156124702+Aboutme-t@users.noreply.github.com>
2024-02-08 01:33:08 +0700

    Create Pandas-DataFrame.py

---
+++ Project-08-Python/Pandas-DataFrame.py
@@ -0,0 +1,27 @@
+## import pandas
+import pandas as pd
+
+## read csv file from pandas
+
+df = pd.read_csv("store.csv")
+
+## clean data 
+## rename colums
+col_name = list(df.columns)
+
+## list comprehension
+clean_col = [name.lower().replace(" ","_").replace("-","_") for name in col_name]
+
+df.columns = clean_col
+
+## data tranfomation
+df[df["country"]== "United States"][["customer_name","country"]]
+
+## query
+df.query("city == 'Los Angeles' and category == 'Furniture'")
+
+## aggregate data
+df.groupby(["segmment","region"])[["sales","profit"]].arr(["sum","mean","std"]).reset_index()
+
+## write csv
+df.to_csv("new_store.csv")
